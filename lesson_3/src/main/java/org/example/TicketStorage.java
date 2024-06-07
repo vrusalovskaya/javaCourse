d3e614ce55f08905a611d1f869107b0ede5f211e
victoria_rusalovskaya <vika.rusalovskaya@gmail.com>
2024-06-07 11:12:06 +0400

    Add TicketStorage class and constructor

---
+++ lesson_3/src/main/java/org/example/TicketStorage.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class TicketStorage {
+
+   private ArrayList<Ticket> storage;
+
+    TicketStorage(){
+        this.storage = new ArrayList<Ticket>();
+        this.storage.add(Ticket.createEmpty());
+        this.storage.add(Ticket.createEmpty());
+        this.storage.add(Ticket.createEmpty());
+        this.storage.add(Ticket.createEmpty());
+        this.storage.add(Ticket.createLimited(new TicketPrice(BigDecimal.valueOf(100), Currency.USD), "Harpa Hall", 111,1749265004L));
+        this.storage.add(Ticket.createLimited(new TicketPrice(BigDecimal.valueOf(200), Currency.USD), "Rudolfinum", 222,1780801004L));
+        this.storage.add(Ticket.createLimited(new TicketPrice(BigDecimal.valueOf(300), Currency.USD), "La Scala", 333,1812337004L));
+        this.storage.add(Ticket.createLimited(new TicketPrice(BigDecimal.valueOf(400), Currency.USD), "SoundBox", 444,1843959404L));
+        this.storage.add(Ticket.create(new TicketPrice(BigDecimal.valueOf(500), Currency.USD), "TuneHub", 555,1875495404L, false, StadiumSector.B, 10.5));
+        this.storage.add(Ticket.create(new TicketPrice(BigDecimal.valueOf(600), Currency.USD), "AriaHall", 666,1907031404L, true, StadiumSector.C, 5.0));
+    }
+}
