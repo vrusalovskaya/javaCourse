9e0a0fd99c7313669df8a50fc6d29e455a3dea40
victoria_rusalovskaya <vika.rusalovskaya@gmail.com>
2024-06-08 20:59:47 +0400

    HW 4 initial commit

---
+++ lesson_4/src/main/java/org/example/TicketPrice.java
@@ -0,0 +1,23 @@
+package org.example;
+import java.math.BigDecimal;
+
+public class TicketPrice {
+
+    private final BigDecimal price;
+    private final Currency currency;
+
+    public TicketPrice(BigDecimal price, Currency currency) {
+
+        if (price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Price must be non-negative");
+        }
+
+        this.price = price;
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return this.price + " " + currency;
+    }
+}
---
+++ lesson_4/src/main/java/org/example/TicketStorage.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class TicketStorage {
+
+    private final ArrayList<Ticket> storage;
+
+    TicketStorage(){
+        this.storage = new ArrayList<Ticket>();
+        this.storage.add(Ticket.createEmpty());
+        this.storage.add(Ticket.createEmpty());
+        this.storage.add(Ticket.createEmpty());
+        this.storage.add(Ticket.createEmpty());
+        this.storage.add(Ticket.createLimited(new TicketPrice(BigDecimal.valueOf(100), Currency.USD), "Harpa Hall", 111,1749265004L));
+        this.storage.add(Ticket.createLimited(new TicketPrice(BigDecimal.valueOf(200), Currency.USD), "Rudolfinum", 222,1780801004L));
+        this.storage.add(Ticket.createLimited(new TicketPrice(BigDecimal.valueOf(300), Currency.USD), "La Scala", 333,1812337004L));
+        this.storage.add(Ticket.createLimited(new TicketPrice(BigDecimal.valueOf(400), Currency.USD), "SoundBox", 444,1843959404L));
+        this.storage.add(Ticket.create(new TicketPrice(BigDecimal.valueOf(500), Currency.USD), "TuneHub", 555,1875495404L, false, StadiumSector.B, 10.5));
+        this.storage.add(Ticket.create(new TicketPrice(BigDecimal.valueOf(600), Currency.USD), "AriaHall", 666,1907031404L, true, StadiumSector.C, 5.0));
+    }
+
+    public Ticket getTicketById(String id){
+        for (Ticket ticket : this.storage) {
+            if (Objects.equals(ticket.getId(), id)){
+                return ticket;
+            }
+        }
+        return null;
+    }
+}
